d1201d4e300c066fa651b0e91e4ae12c3b973574
Filipp Gaponenko <psr1919plus21@gmail.com>
2017-01-03 01:08:25 +0300

    :rocket: Passing varibles from python to html.

--- mysite/personal/views.py
+++ mysite/personal/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def index(request):
   return render(request, 'personal/home.html')
+
+def contact(request):
+  return render(request, 'personal/basic.html', {'content': ['If you want contact me, please send your message to email', 'psr1919plus21@gmail.com']})
