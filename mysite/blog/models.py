d77b7472205b6b19b745a0357a432f886c70b30a
Filipp Gaponenko <psr1919plus21@gmail.com>
2017-03-12 20:14:47 +0300

    :rocket: Play with wyswig.

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -1,11 +1,11 @@
 from __future__ import unicode_literals
-
 from django.db import models
+from froala_editor.fields import FroalaField
 
 class Post(models.Model):
   title = models.CharField(max_length=140)
-  body = models.TextField()
+  body = FroalaField()
   date = models.DateTimeField()
 
-  def __str__(self):
+  def __unicode__(self):
     return self.title
--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'froala_editor'
 ]
 
 MIDDLEWARE = [
@@ -120,3 +121,18 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# WYSWYG settings
+
+
+
+
+
+
+
+
+
+
+
+
+
